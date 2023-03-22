c0ffad65d8abbed545114101e8cdf968d2a8fc5e
Eren Küçük <erenkucuk2002@gmail.com>
2023-03-22 15:58:41 +0300

    clear next.js default code

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,7 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="favicon.ico" />
         </Head>
         <body>
           {getInitColorSchemeScript()}
