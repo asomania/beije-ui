71de85d4cf0ca38f0467b59eb3fe661351e2e99e
Eren Küçük <erenkucuk2002@gmail.com>
2023-03-25 18:50:00 +0300

    footer fix

--- layouts/Footer.tsx
+++ layouts/Footer.tsx
@@ -1,43 +1,175 @@
-import { Grid, Typography } from "@mui/material";
+import {
+  Grid,
+  Typography,
+  Container,
+  Box,
+  TextField,
+  Button,
+} from "@mui/material";
+import {
+  BsLinkedin,
+  BsSpotify,
+  BsTwitter,
+  BsFacebook,
+  BsInstagram,
+} from "react-icons/bs";
+import Image from "next/image";
 
 const Footer = (): JSX.Element => {
+  const footerLinks1: string[] = [
+    "Paketler",
+    "Deneme Paketi",
+    "Ekibimize Katıl",
+  ];
+  const footerLinks2: string[] = [
+    "Blog",
+    "Sıkça Sorulan Sorular",
+    "Biz Kimiz?",
+  ];
+  const socialLinks: string[] = [
+    "Facebook",
+    "Instagram",
+    "Twitter",
+    "Linkedin",
+    "Spotify",
+  ];
+  const socialIcons: JSX.Element[] = [
+    <BsFacebook />,
+    <BsInstagram />,
+    <BsTwitter />,
+    <BsLinkedin />,
+    <BsSpotify />,
+  ];
+
   return (
     <footer
-      style={{ backgroundColor: "#3f51b5", color: "#fff", padding: "1rem" }}
+      style={{
+        color: "#fff",
+        padding: "2rem 0",
+        backgroundColor: "#262626",
+      }}
     >
-      <Grid
-        container
-        spacing={3}
-        justifyContent="space-between"
-        alignItems="center"
-      >
-        <Grid item xs={12} sm={3}>
-          <Typography variant="body1" align="center">
-            © 2023 My Website Name. All rights reserved.
-          </Typography>
+      <Container maxWidth="lg">
+        <Grid container justifyContent="space-between">
+          <Grid item xs={12} sm={5}>
+            <Image
+              src="./footer-image.svg"
+              alt="footer-image"
+              width={50}
+              height={40}
+            />
+            <Box marginTop={2} sx={{ color: "rgba(255, 255, 255, 0.698)" }}>
+              <Typography variant="body1" fontSize={16} fontWeight={500}>
+                Arayı açmayalım!
+              </Typography>
+              <Typography variant="body1" fontSize={14} fontWeight={400}>
+                Aylık e-gazetemiz döngü, yeni ürün ve gelişmelerden haberdar ol.
+              </Typography>
+              <form>
+                <Grid container spacing={2} alignItems="center" marginY={2}>
+                  <Grid item xs={12} sm={9}>
+                    <TextField
+                      label="e-posta adresini giriniz"
+                      type="email"
+                      variant="outlined"
+                      fullWidth
+                      size="small"
+                      InputLabelProps={{
+                        style: {
+                          color: "rgba(255, 255, 255, 0.698)",
+                        },
+                      }}
+                      sx={{
+                        "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline":
+                          {
+                            borderColor: "rgba(255, 255, 255, 0.698)",
+                          },
+                      }}
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={3}>
+                    <Button
+                      variant="contained"
+                      type="submit"
+                      sx={{
+                        borderRadius: 20,
+                        backgroundColor: "White",
+                        color: "Black",
+                        "&:hover": {
+                          backgroundColor: "#262626",
+                        },
+                      }}
+                    >
+                      Gönder
+                    </Button>
+                  </Grid>
+                </Grid>
+              </form>
+              <Typography variant="body1" fontSize={12} fontWeight={300}>
+                Abone olarak, beije KVKK ve Gizlilik Politikası'nı kabul ediyor
+                ve beije'den haber almayı onaylıyorum.
+              </Typography>
+            </Box>
+          </Grid>
+          <Grid item xs={12} sm={1}></Grid>
+          <Grid item xs={12} sm={2}>
+            <Box>
+              {footerLinks1.map((link) => (
+                <Typography variant="body1" marginBottom={2}>
+                  <a
+                    href="/"
+                    style={{
+                      color: "#fff",
+                      textDecoration: "none",
+                    }}
+                  >
+                    {link}
+                  </a>
+                </Typography>
+              ))}
+            </Box>
+          </Grid>
+          <Grid item xs={12} sm={2}>
+            <Box>
+              {footerLinks2.map((link) => (
+                <Typography variant="body1" marginBottom={2}>
+                  <a href="/" style={{ color: "#fff", textDecoration: "none" }}>
+                    {link}
+                  </a>
+                </Typography>
+              ))}
+            </Box>
+          </Grid>
+          <Grid item xs={12} sm={2}>
+            <Box>
+              {socialLinks.map((link, index) => (
+                <Typography variant="body1" fontSize={25}>
+                  <a
+                    href="/"
+                    style={{
+                      color: "#fff",
+                      textDecoration: "none",
+                      display: "flex",
+                      alignItems: "center",
+                      marginBottom: "1.5rem",
+                      gap: "1rem",
+                    }}
+                  >
+                    {socialIcons[index]}
+                    <span style={{ fontSize: "16px" }}>{link}</span>
+                  </a>
+                </Typography>
+              ))}
+            </Box>
+            {/* <Typography variant="body1" fontSize={25} justifyContent="center">
+              <a href="/" style={{ color: "#fff", textDecoration: "none" }}>
+                <CgFacebook />{" "}
+                <span style={{ fontSize: "16px" }}>Facebook</span>
+              </a>
+            </Typography> */}
+          </Grid>
         </Grid>
-        <Grid item xs={12} sm={3}>
-          <Typography variant="body1" align="center">
-            <a href="/" style={{ color: "#fff", textDecoration: "none" }}>
-              Privacy Policy
-            </a>
-          </Typography>
-        </Grid>
-        <Grid item xs={12} sm={3}>
-          <Typography variant="body1" align="center">
-            <a href="/" style={{ color: "#fff", textDecoration: "none" }}>
-              Terms of Use
-            </a>
-          </Typography>
-        </Grid>
-        <Grid item xs={12} sm={3}>
-          <Typography variant="body1" align="center">
-            <a href="/" style={{ color: "#fff", textDecoration: "none" }}>
-              Contact Us
-            </a>
-          </Typography>
-        </Grid>
-      </Grid>
+      </Container>
     </footer>
   );
 };
